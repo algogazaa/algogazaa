d13bd1d847ea2c3833261a7e92302a601d2b7cfe
송민진 <m_6595@naver.com>
2022-09-27 16:46:08 +0900

    no message

---
+++ Greedy/백준-국회의원선거/220927_송민진.py
@@ -0,0 +1,12 @@
+n = int(input())
+dasom = int(input())
+candidates = [int(input()) for _ in range(n-1)]
+answer = 0
+
+if candidates:
+    while dasom <= max(candidates):
+        candidates[candidates.index(max(candidates))] -= 1
+        dasom += 1
+        answer += 1
+
+print(answer)
---
+++ Greedy/백준-잃어버린괄호/220927_송민진.py
@@ -0,0 +1,27 @@
+calc_str = input()
+answer = 0
+
+operation_list = []
+
+for letter in calc_str:
+    if letter == '+' or letter == '-':
+        operation_list.append(letter)
+
+calc_str = calc_str.replace('+', '-')
+num_list = list(map(int, calc_str.split('-')))
+
+answer += num_list[0]
+num_list.remove(num_list[0])
+
+for i in range(len(operation_list)):
+    if i != 0 and operation_list[i] == '+' and operation_list[i-1] == '-':
+        operation_list[i] = '-'
+    if num_list:
+        if operation_list[i] == '+':
+            answer += num_list[0]
+            num_list.remove(num_list[0])
+        else:
+            answer -= num_list[0]
+            num_list.remove(num_list[0])
+
+print(answer)

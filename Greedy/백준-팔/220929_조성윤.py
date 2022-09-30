788544bb3b569f75df3d05a67cf073ddfa2421c6
Cho-El <syzzang21c@gmail.com>
2022-09-30 16:19:11 +0900

    Update 220929_조성윤.py

---
+++ Greedy/백준-팔/220929_조성윤.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+l,r = input().split()
+
+result = 0
+
+if len(l) != len(r):
+    print(0)
+
+else:
+    if l[0] != r[0]:
+        print(0)
+    else:
+        if l[0] == '8':
+            result += 1
+        for i in range(1, len(l)):
+            if l[i] != r[i]:
+                break
+            else:
+                if l[i] == '8':
+                    result += 1
+        print(result)
+                

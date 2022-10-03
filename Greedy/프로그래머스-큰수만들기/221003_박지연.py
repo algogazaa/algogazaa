c5bb9a83cbae4f4cbf14b92248c49e924946e873
Jeeyeonn <qkrwldus3927@naver.com>
2022-10-03 10:13:20 +0900

    Update 221003_박지연.py

--- Greedy/프로그래머스-큰수만들기/221003_박지연.py
+++ Greedy/프로그래머스-큰수만들기/221003_박지연.py
@@ -1,27 +1,46 @@
+# def solution(number, k):
+#     number_list = list(number)
+#     cut_list = number_list[:k+1]
+#
+#     max_first = max(cut_list)
+#     max_index = cut_list.index(max_first)
+#
+#     a = number_list[max_index:]
+#     i = 1
+#
+#     while len(a) != len(number) - k:
+#         if i == len(a)-1:
+#             break
+#         if a[i] < a[i+1]:
+#             a = a[:i] + a[i+1:]
+#
+#         i += 1
+#
+#     if len(a) > len(number) - k:
+#         a = a[:len(number)-k]
+#
+#     return ''.join(a)
+
+
 def solution(number, k):
-    number_list = list(number)
-    cut_list = number_list[:k+1]
+    number = list(number)
+    result = [number.pop(0)]
+    for n in number:
+        if result[-1] < n:
+            while result and result[-1] < n and k > 0:
+                result.pop()
+                k -= 1
+            result.append(n)
+        elif k == 0 or result[-1] >= n:
+            result.append(n)
 
-    max_first = max(cut_list)
-    max_index = cut_list.index(max_first)
+    if k:
+        result = result[:-k]
+    answer = ''.join(result)
 
-    a = number_list[max_index:]
-    i = 1
-
-    while len(a) != len(number) - k:
-        if i == len(a)-1:
-            break
-        if a[i] < a[i+1]:
-            a = a[:i] + a[i+1:]
-
-        i += 1
-
-    if len(a) > len(number) - k:
-        a = a[:len(number)-k]
-
-    return ''.join(a)
+    return answer
 
 a = "4177252841"
 b = "654321"
 c = "1234567"
-print(solution(c, 4))
+print(solution(a, 4))

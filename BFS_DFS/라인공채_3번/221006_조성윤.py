7bffbcc140066e1fd335879a6d722d234267e9bc
Cho-El <syzzang21c@gmail.com>
2022-10-06 09:12:37 +0900

    221006문제 조성윤

---
+++ BFS_DFS/라인공채_3번/221006_조성윤.py
@@ -0,0 +1,56 @@
+from collections import deque
+def solution(n: int, m: int, fires: List[List[int]], ices: List[List[int]]) -> List[List[int]]:
+    answer = []
+    graph = [[0] * n for _ in range(n)]
+
+    for f in fires:
+        temp = [f[0]-1, f[1]-1,m]
+        visited = [[False] * n for _ in range(n)]
+        bfs(n,temp, graph,visited, 1)
+        
+    for ic in ices:
+        temp = [ic[0]-1, ic[1]-1,m]
+        visited = [[False] * n for _ in range(n)]
+        bfs(n,temp, graph,visited, -1)
+    
+    for g in graph:
+        answer.append(g)
+    return answer
+
+def bfs(n,start, graph, visited, changeTemp):
+    q = deque([start])
+    visited[start[0]][start[1]] = True
+    if changeTemp == 1:
+        graph[start[0]][start[1]] += start[2]
+    else:
+        graph[start[0]][start[1]] -= start[2]
+    fire_dx = [0,0,1,-1,1,-1,1,-1]
+    fire_dy = [1,-1,0,0,-1,1,1,-1]
+    ice_dx = [0,0,1,-1]
+    ice_dy = [1,-1,0,0]
+    while q:
+        x,y,canMove = q.popleft()
+        if changeTemp == 1:
+            for i in range(len(fire_dx)):
+                nx = fire_dx[i] + x
+                ny = fire_dy[i] + y
+                if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and canMove != 0:
+                    graph[nx][ny] += canMove
+                    visited[nx][ny] = True
+                    q.append([nx,ny,canMove - 1])
+        else:
+            for i in range(len(ice_dx)):
+                nx = ice_dx[i] + x
+                ny = ice_dy[i] + y
+                if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny] and canMove != 0:
+                    graph[nx][ny] -= canMove
+                    visited[nx][ny] = True
+                    q.append([nx,ny,canMove - 1])
+
+if __name__ == "__main__":
+    n = [5,3]
+    m = [3,2]
+    fires  = [[[5,5],[1,3],[5,2]],[[1,1]]]
+    ices = [[[1,5],[3,2]],[[3,3]]]
+    for a,b,c,d in zip(n,m,fires,ices):
+        print(solution(a,b,c,d))
---
+++ BFS_DFS/백준-토마토/221005_조성윤.py
@@ -0,0 +1,47 @@
+#
+import sys
+from collections import deque
+input = sys.stdin.readline
+m,n,h = map(int, input().split())
+graph = []
+for _ in range(h):
+    temp = []
+    for _ in range(n):
+        temp.append(list(map(int,input().split())))
+    graph.append(temp)
+
+start = []
+for z in range(h):
+    for x in range(n):
+        for y in range(m):
+            if graph[z][x][y] == 1:
+                start.append([z,x,y])
+
+def bfs(start):
+    q = deque(start)
+    dx = [1,-1,0,0,0,0]
+    dy = [0,0,1,-1,0,0]
+    dz = [0,0,0,0,1,-1]
+    while q:
+        z,x,y = q.popleft()
+        for i in range(6):
+            nz = z + dz[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nz < h and 0 <= nx < n and 0 <= ny < m and graph[nz][nx][ny] == 0:
+                graph[nz][nx][ny] = graph[z][x][y] + 1
+                q.append([nz,nx,ny])
+                
+bfs(start)
+result = 0
+for i in graph:
+    for j in i:
+        if 0 in j:
+            print(-1)
+            result = -1
+            break
+        result = max(result, max(j))
+    if result == -1:
+        break
+else:
+    print(result-1)

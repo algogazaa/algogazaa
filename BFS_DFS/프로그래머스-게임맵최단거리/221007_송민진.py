13abcb0ba32b3ec40716b816548ba8352b49bc64
송민진 <100582309+deingvelop@users.noreply.github.com>
2022-10-07 13:34:00 +0900

    Create 221007_송민진.py

---
+++ BFS_DFS/프로그래머스-게임맵최단거리/221007_송민진.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def solution(maps):
+    x, y = 0, 0
+    n = len(maps)
+    m = len(maps[0])
+    queue = deque()
+    queue.append((x, y))
+
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    while queue:
+        x, y = queue.popleft()
+        for di in range(4):
+            nx = x + dx[di]
+            ny = y + dy[di]
+            if nx >= len(maps) or ny >= len(maps[0]) or nx < 0 or ny < 0:
+                continue
+            if maps[nx][ny] == 0:
+                continue
+            if maps[nx][ny] == 1:
+                maps[nx][ny] = maps[x][y] + 1
+                queue.append((nx, ny))
+    if maps[n-1][m-1] in [0, 1]: return -1
+    else: return maps[n-1][m-1]

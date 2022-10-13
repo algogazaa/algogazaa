49053cae0eace3512960353f15fa4db4e1c94777
LEE HYE JIN <108391677+Hiluxy@users.noreply.github.com>
2022-10-13 22:16:06 +0900

    Update 221011_이혜진.py

--- BFS_DFS/백준-바이러스/221011_이혜진.py
+++ BFS_DFS/백준-바이러스/221011_이혜진.py
@@ -1,4 +1,3 @@
-#런타임 에러
 
 from collections import deque
 
@@ -7,31 +6,35 @@ num=int(input())
 n=int(input())
 dic={}
 for i in range(num):
-    dic[i+1]=set()
+    dic[i]=set()
 
 for _ in range(n):
     a,b=map(int,input().split())
-    dic[a].add(b)
-    dic[b].add(a)
+    dic[a-1].add(b-1)
+    dic[b-1].add(a-1)
 
-#dic={1: {2, 5}, 2: {1, 3, 5}, 3: {2}, 4: {7}, 5: {1, 2, 6}, 6: {5}, 7: {4}}
 
-visited=[False]*n
+#노드 0번부터 시작으로 계산
+#dic={0: {1, 4}, 1: {0, 2, 4}, 2: {1}, 3: {6}, 4: {0, 1, 5}, 5: {4}, 6: {3}}
+visited=[False]*num #해결!! n,num 변수명 헷갈림
 
 def bfs(start,visited):
     que=deque()
     que.append(start)
-    visited[start]==True
+    visited[start]=True
+
     while que:
         node=que.popleft()
-        for val in dic[node]:
-            if visited[val-1]==False:
+        for val in dic[node]: 
+            if visited[val]==False:
                 que.append(val)
-                visited[val-1]=True
+                visited[val]=True
 
-bfs(1,visited)
+bfs(0,visited)
+#visited=[True, True, True, False, True, True, False]
 cnt=0
 for v in visited:
     if v==True:
         cnt+=1
 print(cnt-1)
+

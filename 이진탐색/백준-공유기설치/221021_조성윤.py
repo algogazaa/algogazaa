abc0c44c15eb597e8003f6fe519410954c5c4237
Cho-El <syzzang21c@gmail.com>
2022-10-21 12:48:24 +0900

    Update 221021_조성윤.py

---
+++ 이진탐색/백준-공유기설치/221021_조성윤.py
@@ -0,0 +1,28 @@
+#
+import sys
+input = sys.stdin.readline
+n,c = map(int, input().split())
+wifi = []
+for _ in range(n):
+    wifi.append(int(input()))
+wifi.sort()
+start = 1
+end = wifi[-1] - wifi[0]
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    curHouse = wifi[0]
+    cnt = 1
+    for i in range(1,len(wifi)):
+        if wifi[i] >= curHouse + mid:
+            cnt += 1
+            curHouse = wifi[i]
+    
+    if cnt >= c:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+
+print(result)

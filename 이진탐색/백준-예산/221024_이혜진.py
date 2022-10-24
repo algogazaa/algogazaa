ff0f26d36e9860afe83a70076b83e71e5f518d29
LEE HYE JIN <108391677+Hiluxy@users.noreply.github.com>
2022-10-24 12:25:40 +0900

    Create 221024_이혜진.py

---
+++ 이진탐색/백준-예산/221024_이혜진.py
@@ -0,0 +1,32 @@
+#시간초과?
+import sys
+input = sys.stdin.readline
+n=int(input())
+arr=list(map(int,input().split()))
+target=int(input())
+
+def b(arr,target,start,end):
+    if sum(arr)<=target:
+        return max(arr)
+
+    while start<=end:
+        mid=(start+end)//2
+        total=0
+        for i in arr:
+            if i<=mid:
+                total+=i
+            else:
+                total+=mid
+        
+        if total==target:
+            result=mid
+        elif total<target:
+            start=mid+1
+            result=mid
+        else:
+            end=mid-1
+
+    return result
+
+
+print(b(arr,target,0,max(arr)))

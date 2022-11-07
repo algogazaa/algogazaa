d3f981371c9e0483626967ae88d21dedec570f62
Cho-El <syzzang21c@gmail.com>
2022-11-07 17:01:40 +0900

    DP

---
+++ DP/계단오르기/221107_조성윤.py
@@ -0,0 +1,16 @@
+# dp
+import sys
+n = int(input())
+stair = [0] * (n+2)
+dp = [0] * (n+2)
+for i in range(n):
+	stair[i] = int(sys.stdin.readline().rstrip())
+
+dp[0] = stair[0]
+dp[1] = stair[0] + stair[1]
+dp[2] = max(stair[0] + stair[2], stair[1] + stair[2])
+
+for i in range(3, n):
+	dp[i] = max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i])
+
+print(dp[n-1])

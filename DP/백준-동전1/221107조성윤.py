240d891867b1889e456d765b382fe38ff3554133
Cho-El <68215871+Cho-El@users.noreply.github.com>
2022-11-10 13:11:40 +0900

    Update 221107조성윤.py

---
+++ DP/백준-동전1/221107조성윤.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+
+dp = [0] * (k+1)
+dp[0] = 1
+for coin in coins:
+    for i in range(coin, k+1):
+        possible_cases = dp[i - coin]
+        dp[i] += possible_cases
+
+print(dp[k])

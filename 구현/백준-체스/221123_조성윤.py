5128d886701fee92698ebae2dfc94152a9a14e14
Cho-El <syzzang21c@gmail.com>
2022-11-24 14:45:45 +0900

    Update 221123_조성윤.py

--- 구현/백준-체스/221123_조성윤.py
+++ 구현/백준-체스/221123_조성윤.py
@@ -5,7 +5,24 @@ n,m = map(int, input().split())
 q = list(map(int, input().split()))
 k = list(map(int, input().split()))
 p = list(map(int, input().split()))
-graph = [[False] * m for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
 def dfsQ(x,y,direction):
+    visited[x][y] = True
     
+    if direction == 1:
+        return
+    elif direction == 2:
+        return
+    elif direction == 3:
+        return
+    elif direction == 4:
+        return
+    elif direction == 5:
+        return
+    elif direction == 6:
+        return
+    elif direction == 7:
+        return
+    elif direction == 8:
+        return

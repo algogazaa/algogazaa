d39db49704efde55762b1a90fe874d8c15ba2fb2
Cho-El <syzzang21c@gmail.com>
2022-11-28 11:24:43 +0900

    221128

---
+++ 구현/백준-지뢰찾기/221127_조성윤.py
@@ -0,0 +1,34 @@
+#
+import sys
+input = sys.stdin.readline
+n = int(input())
+mineGraph = [list(input().rstrip()) for _ in range(n)]
+openGraph = [list(input().rstrip()) for _ in range(n)]
+findMine = False
+
+dx = [0,0,1,-1,1,-1,-1,1]
+dy = [1,-1,0,0,1,-1,1,-1]
+for x in range(n):
+    for y in range(n):
+        if openGraph[x][y] == "x":
+            # 만약 x칸인데 지뢰라면
+            if mineGraph[x][y] == "*":
+                findMine = True
+                continue
+            
+            cnt = 0
+            for i in range(8):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < n and 0 <= ny < n:
+                    if mineGraph[nx][ny] == "*":
+                        cnt += 1
+            openGraph[x][y] = str(cnt)
+
+if findMine:
+    for x in range(n):
+        for y in range(n):
+            if mineGraph[x][y] == "*":
+                openGraph[x][y] = "*"
+for o in openGraph:
+    print(''.join(o))
--- 구현/백준-체스/221123_조성윤.py
+++ 구현/백준-체스/221123_조성윤.py
@@ -5,24 +5,54 @@ n,m = map(int, input().split())
 q = list(map(int, input().split()))
 k = list(map(int, input().split()))
 p = list(map(int, input().split()))
-visited = [[False] * m for _ in range(n)]
+board = [['0'] * m for _ in range(n)]
 
-def dfsQ(x,y,direction):
-    visited[x][y] = True
+for i in range(1,len(q),2):
+    board[q[i] - 1][q[i + 1] - 1] = 'q'
+
+for i in range(1, len(k), 2):
+    board[k[i] - 1][k[i + 1] - 1] = 'k'
+
+for i in range(1, len(p), 2):
+    board[p[i] - 1][p[i + 1] - 1] = 'p'
+
+def dfsQ(x,y):
+    dx = [0,0,1,-1,1,-1,1,-1]
+    dy = [1,-1,0,0,1,1,-1,-1]
+
     
-    if direction == 1:
-        return
-    elif direction == 2:
-        return
-    elif direction == 3:
-        return
-    elif direction == 4:
-        return
-    elif direction == 5:
-        return
-    elif direction == 6:
-        return
-    elif direction == 7:
-        return
-    elif direction == 8:
-        return
+    for i in range(8):
+        nx,ny = x,y
+        while True:
+            nx += dx[i]
+            ny += dy[i]
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] != 'p' and board[nx][ny] != 'k' and board[nx][ny] != 'q':
+                board[nx][ny] = '1'
+                continue
+            else:
+                break
+    return
+
+def dfsK(x, y):
+    dx = [-1,1,-2,2,-2,2,-1,1]
+    dy = [-2,-2,-1,-1,1,1,2,2]
+
+    
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m and board[nx][ny] != 'p' and board[nx][ny] != 'k' and board[nx][ny] != 'q':
+            board[nx][ny] = '1'
+    
+    return
+
+for i in range(1,len(q),2):
+    dfsQ(q[i] - 1, q[i + 1] - 1)
+
+for i in range(1, len(k), 2):
+    dfsK(k[i] - 1, k[i + 1] - 1)
+    
+result = 0
+for b in board:
+    result += b.count("0")
+print(result)

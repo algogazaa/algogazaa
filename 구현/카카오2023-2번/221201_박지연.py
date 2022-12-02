d3b2545f47547676087756cc127d27069dcd8c68
Jeeyeonn <qkrwldus3927@naver.com>
2022-12-02 10:13:45 +0900

    Update 221201_박지연.py

--- 구현/카카오2023-2번/221201_박지연.py
+++ 구현/카카오2023-2번/221201_박지연.py
@@ -1,60 +1,33 @@
 def solution(cap, n, deliveries, pickups):
-    index = 0
-    i = n - 1
-    d_index = n - 1
-    p_index = n - 1
-    while True:
-        while True:
-            if deliveries[i] == 0 and pickups[i] == 0:
-                if i < 0:
-                    return index
-                i -= 1
+    answer = 0
+    d_cnt, p_cnt = 0, 0
+    visited = []
+
+    for i in reversed(range(n)):
+        if deliveries[i] != 0:
+            visited.append(i+1)
+            d_cnt += deliveries[i]
+        if pickups[i] != 0:
+            visited.append(i+1)
+            p_cnt += pickups[i]
+
+        if d_cnt > cap or p_cnt > cap:
+            answer += max(visited) * 2
+            visited = []
+            if d_cnt > cap:
+                visited.append(i+1)
+                d_cnt -= cap
             else:
-                break
-
-        d_num = 0
-        p_num = 0
-        index += ((i + 1) * 2)
-
-        while True:
-            if d_num >= cap:
-                break
-
-            if d_index < 0:
-                break
-
-            if deliveries[d_index] < (cap - d_num):
-                d_num += deliveries[d_index]
-                deliveries[d_index] = 0
-                d_index -= 1
-            elif deliveries[d_index] == (cap - d_num):
-                deliveries[d_index] = 0
-                d_index -= 1
-                break
+                d_cnt = 0
+            if p_cnt > cap:
+                visited.append(i+1)
+                p_cnt -= cap
             else:
-                deliveries[d_index] -= cap - d_num
-                break
+                p_cnt = 0
 
-        while True:
-            if p_num >= cap:
-                break
-
-            if p_index < 0:
-                break
-
-            if pickups[p_index] < (cap - p_num):
-                p_num += pickups[p_index]
-                pickups[p_index] = 0
-                p_index -= 1
-            elif pickups[p_index] == (cap - p_num):
-                pickups[p_index] = 0
-                p_index -= 1
-                break
-            else:
-                pickups[p_index] -= cap - p_num
-                break
-
-    return index
+        if i == 0 and len(visited) != 0:
+            answer += max(visited) * 2
+    return answer
 
 
 print(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]))

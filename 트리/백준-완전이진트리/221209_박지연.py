720ce0a14fe8e808f8d6b2ffae842c976d26b0a3
Jeeyeonn <qkrwldus3927@naver.com>
2022-12-09 10:07:11 +0900

    Update 221209_박지연.py

---
+++ 트리/백준-완전이진트리/221209_박지연.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+n = int(input())
+node = list(input().split())
+answer = [[0]for _ in range(n)]
+q = deque()
+q.append([node, 0])
+
+while q:
+    i, cnt = q.popleft()
+    left, middle, right = i[:len(i)//2], i[len(i)//2], i[len(i)//2+1:]
+    answer[cnt].append(middle)
+    if len(left) != 1:
+        q.append([left, cnt + 1])
+        q.append([right, cnt + 1])
+    else:
+        answer[cnt+1].append(i[0])
+        answer[cnt+1].append(i[2])
+
+for i in answer:
+    i = i[1:]
+    print(' '.join(i))
